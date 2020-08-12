de1005c442c3fa468d17ed33955c7b59981f6dca
Jen Gray <69304519+jgray13@users.noreply.github.com>
2020-08-12 22:23:15 +0100

    Update files
    Start working on javaScript and user input.

--- dice.js
+++ dice.js
@@ -3,8 +3,121 @@ function rollTheDice(){
   lowest_number = 1;
   dice1 = Math.floor((Math.random()*highest_number)+lowest_number);   
   dice2 = Math.floor((Math.random()*highest_number)+lowest_number);   
-  document.getElementById('output').innerHTML = 'Dice 1: ' + dice1 + '<br>Dice 2: ' + dice2;   
-  document.getElementById('totalRoll').innerHTML = 'Total: '+(dice1+dice2);
+  document.getElementById('output1').innerHTML = dice1;   
+  document.getElementById('output2').innerHTML = dice2;
+  
+  console.log(dice1);
+  console.log(dice2);
+  
+  var d1 = document.getElementById("dice1");
+  var d2 = document.getElementById("dice2");
+  var d3 = document.getElementById("dice3");
+  var d4 = document.getElementById("dice4");
+  var d5 = document.getElementById("dice5");
+  var d6 = document.getElementById("dice6");
+  var d7 = document.getElementById("dice7");
+  var d8 = document.getElementById("dice8");
+  var d9 = document.getElementById("dice9");
+  var d10 = document.getElementById("dice10");
+  var d11 = document.getElementById("dice11");
+  var d12 = document.getElementById("dice12");
+  if (dice1 === 1) {
+	d1.style.display = "flex";
+	d2.style.display = "none";
+	d3.style.display = "none";
+	d4.style.display = "none";
+	d5.style.display = "none";
+	d6.style.display = "none";
+  } else if (dice1 === 2) {
+	  d1.style.display = "none";
+	  d2.style.display = "flex";
+	  d3.style.display = "none";
+	  d4.style.display = "none";
+	  d5.style.display = "none";
+	  d6.style.display = "none";
+  } else if (dice1 === 3) {
+	   d1.style.display = "none";
+	   d2.style.display = "none";
+	   d3.style.display = "flex";
+	   d4.style.display = "none";
+	   d5.style.display = "none";
+	   d6.style.display = "none";
+  } else if (dice1 === 4) {
+	  d1.style.display = "none";
+	  d2.style.display = "none";
+	  d3.style.display = "none";
+	  d4.style.display = "flex";
+	  d5.style.display = "none";
+	  d6.style.display = "none";
+  } else if (dice1 === 5) {
+	  d1.style.display = "none";
+	  d2.style.display = "none";
+	  d3.style.display = "none";
+	  d4.style.display = "none";
+	  d5.style.display = "flex";
+	  d6.style.display = "none";
+  } else if (dice1 === 6) {
+	  d1.style.display = "none";
+	  d2.style.display = "none";
+	  d3.style.display = "none";
+	  d4.style.display = "none";
+	  d5.style.display = "none";
+	  d6.style.display = "flex";
+  }
+    if (dice2 === 1) {
+	d7.style.display = "flex";
+	d8.style.display = "none";
+	d9.style.display = "none";
+	d10.style.display = "none";
+	d11.style.display = "none";
+	d12.style.display = "none";
+  } else if (dice2 === 2) {
+	  d7.style.display = "none";
+	  d8.style.display = "flex";
+	  d9.style.display = "none";
+	  d10.style.display = "none";
+	  d11.style.display = "none";
+	  d12.style.display = "none";
+  } else if (dice2 === 3) {
+	   d7.style.display = "none";
+	   d8.style.display = "none";
+	   d9.style.display = "flex";
+	   d10.style.display = "none";
+	   d11.style.display = "none";
+	   d12.style.display = "none";
+  } else if (dice2 === 4) {
+	  d7.style.display = "none";
+	  d8.style.display = "none";
+	  d9.style.display = "none";
+	  d10.style.display = "flex";
+	  d11.style.display = "none";
+	  d12.style.display = "none";
+  } else if (dice2 === 5) {
+	  d7.style.display = "none";
+	  d8.style.display = "none";
+	  d9.style.display = "none";
+	  d10.style.display = "none";
+	  d11.style.display = "flex";
+	  d12.style.display = "none";
+  } else if (dice2 === 6) {
+	  d7.style.display = "none";
+	  d8.style.display = "none";
+	  d9.style.display = "none";
+	  d10.style.display = "none";
+	  d11.style.display = "none";
+	  d12.style.display = "flex";
+  }
+
+  var valEntered = document.getElementById("diceAnswer").value;
+  valEntered.onkeyup = userInput();
+  function userInput() {
+	  var numEntered = parseInt(valEntered);
+	  console.log(valEntered);
+	  console.log(numEntered);
+	  if (numEntered === dice1.value + dice2.value) {
+		  
+	  }
+  }
 }
 
 
